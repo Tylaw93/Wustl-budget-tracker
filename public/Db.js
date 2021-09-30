d29a37d60c69bfc41e6e9e830095d7024a1ca4a2
Tyler Lawson <tylaw93@yahoo.com>
2021-09-30 17:06:52 -0500

    Revert "Testing"
    This reverts commit 377f11f822481d7054ea9ee8c99a7f343de33db6.

--- public/Db.js
+++ public/Db.js
@@ -1,57 +1,51 @@
 let db;
+const request = indexedDB.open("budget", 1); 
 
-const request = indexedDB.open("BudgetStore", 1);
-
-// https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB#opening_a_database
-request.onupgradeneeded = function(e) {
-  const db = request.result;
-  db.createObjectStore("BudgetStore", { autoIncrement: true });
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
 };
 
-request.onerror = function(e) {
-  console.log("There was an error");
-};
-
-// See 19-PWA\01-Activities\13-Stu_Caching_Fetching_Files\Solved\public\assets\js
-request.onsuccess = function(e) {
-    // https://offering.solutions/blog/articles/2018/11/21/online-and-offline-sync-with-angular-and-indexeddb/
-  db = e.target.result;
-  if (navigator.onLine){
-    checkDB();
-  }
-}
-
-function checkDB(){
-  // const db = event.target.result;
-  const tx = db.transaction(["BudgetStore"], "readwrite");
-  const store = tx.objectStore("BudgetStore");
-  const getData = store.getAll();
-  getData.onsuccess = function(){
-    if (getData.result.length>0){
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getData.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      // return the response as a json
-      .then(res=>res.json())
-      .then(()=>{
-        const tx = db.transaction(["BudgetStore"], "readwrite");
-        const store = tx.objectStore("BudgetStore");
-        store.clear();
-      })
+request.onsuccess = (event) => {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
     }
-  }
-}
+};
 
-// See Line 139 of ../index.js
-function saveRecord(record){
-  const tx = db.transaction(["BudgetStore"],"readwrite");
-  const store = tx.objectStore("BudgetStore");
-  store.add(record)
-}
+request.onerror = (event) => {
+    console.log(event.target.errorCode);
+}; 
 
-window.addEventListener("online",checkDB)
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*", 
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+};
+
+window.addEventListener("online", checkDatabase);
